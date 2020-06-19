a85ecb557386e0f85d0a6791465bd418ab05fab0
刘新辉 <liunewshine@qq.com>
2020-06-19 15:44:44 +0800

    升级依赖库版本

--- template-main/src/main/java/com/step/template/main/TemplateApplication.java
+++ template-main/src/main/java/com/step/template/main/TemplateApplication.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,7 +23,7 @@ public class TemplateApplication implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-        log.info("-------------------平台启动成功-------------------");
+        log.info(AnsiOutput.toString(AnsiColor.GREEN, "-------------------平台启动成功-------------------"));
     }
 
 }
